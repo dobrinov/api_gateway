4ce2ddfb3599fc03dfd1136050d51062ea502568
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-05-04 19:50:08 +0300

    Implement requests with promises

--- application.rb
+++ application.rb
@@ -1,23 +1,16 @@
 require 'sinatra'
+require 'promise'
 require 'json'
 
 require_relative 'microservices/authentication_service'
 require_relative 'microservices/products_service'
 
 get '/products' do
-  session  = AuthenticationService.session
-  products = ProductsService.products(session)
+  session  = promise { AuthenticationService.session }
+  products = promise { ProductsService.products(session) }
 
   {
     session:  session,
     products: products
   }.to_json
 end
-
-# require 'promise'
-# require 'future'
-
-# y = future  { sleep(2) && 6 * 7 }
-# x = promise { 1 + 2 }
-
-# puts x + y
