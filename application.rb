cd31ad421f4046e511aa65008d02f7190353f733
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2016-06-06 00:49:45 +0300

    Use futures instead of promises

--- application.rb
+++ application.rb
@@ -1,16 +1,18 @@
 require 'sinatra'
-require 'promise'
+require 'future'
 require 'json'
 
 require_relative 'microservices/authentication_service'
 require_relative 'microservices/products_service'
 
 get '/products' do
-  session  = promise { AuthenticationService.session }
-  products = promise { ProductsService.products(session) }
+  time = Time.now.to_i
+  session  = future { AuthenticationService.session }; nil;
+  products = future { ProductsService.products(session) }; nil;
 
   {
-    session:  session,
-    products: products
+    session:    session,
+    products:   products,
+    time: time
   }.to_json
 end
